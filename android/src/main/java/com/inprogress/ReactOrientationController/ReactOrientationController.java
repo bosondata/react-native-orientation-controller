4ef3c040ceb078f5aeac927bcd846cd269875d48
messense <messense@icloud.com>
2020-04-06 16:27:21 +0800

    Remove createJSModules override

--- android/src/main/java/com/inprogress/ReactOrientationController/ReactOrientationController.java
+++ android/src/main/java/com/inprogress/ReactOrientationController/ReactOrientationController.java
@@ -30,7 +30,6 @@ public class ReactOrientationController implements ReactPackage {
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
